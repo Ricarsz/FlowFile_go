764c7665fdeed6e5b06569995af0db288dfeb402
李嘉斯 <LJS0102cs@outlook.com>
2026-08-29 21:49:59 +0800

    server-store-end

--- server.go
+++ server.go
@@ -1,36 +1,88 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"sync"
+
 	"github.com/Ricarse/fileFlowSystem/p2p"
 )
 
 type FileServer struct {
-	FileServerOpts FileServerOpts
-	Store          *Store
-	Peers          map[string]p2p.Peer
-	PeerLock       sync.Mutex
+	fileServerOpts FileServerOpts
+	store          *Store
+	peers          map[string]p2p.Peer
+	peerLock       sync.Mutex
 }
 
 func NewFileServer(opts FileServerOpts) *FileServer {
 	return &FileServer{
-		FileServerOpts: opts,
-		Store: NewStore(StoreOpts{Root: opts.StorageRoot,
+		fileServerOpts: opts,
+		store: NewStore(StoreOpts{Root: opts.StorageRoot,
 			PathTransformFunc: opts.PathTransformFunc}),
-		Peers: make(map[string]p2p.Peer),
+		peers: make(map[string]p2p.Peer),
 	}
 }
 
 type FileServerOpts struct {
 	PathTransformFunc PathTransformFunc
 	StorageRoot       string
-	Transport         p2p.Transport
+	Transport         *p2p.TCPTransport
 	BootstrapNodes    []string
 }
 
-func (s *FileServer)OnPeer(p p2p.Peer)error{
-	s.PeerLock.Lock()
-	defer s.PeerLock.Unlock()
-	s.Peers[p.Conn().RemoteAddr().String()]=p
+func (s *FileServer) OnPeer(p p2p.Peer) error {
+	s.peerLock.Lock()
+	defer s.peerLock.Unlock()
+	s.peers[p.Conn().RemoteAddr().String()] = p
+	return nil
+}
+
+func (s *FileServer) loop() error {
+	for {
+		select {
+		case rpc := <-s.fileServerOpts.Transport.Consume():
+			s.store.Write("from_"+rpc.From.String(),
+				bytes.NewReader(rpc.Payload))
+		}
+	}
+}
+
+func (s *FileServer) bootstrapNetwork() error {
+	for _, addr := range s.fileServerOpts.BootstrapNodes {
+		if addr == "" {
+			continue
+		}
+		go func(a string) { s.fileServerOpts.Transport.Dial(a) }(addr)
+	}
+	return nil
+}
+
+func (s *FileServer) broadcastNetwork(key string, b []byte) error {
+	for _, peer := range s.peers {
+		if err := peer.Send(b); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *FileServer) Start() error {
+	s.fileServerOpts.Transport.OnPeer = s.OnPeer
+	go s.fileServerOpts.Transport.ListenAndAccept()
+	s.bootstrapNetwork()
+	s.loop()
+	return nil
+}
+
+func (s *FileServer) Store(key string, r io.Reader) error {
+	b, _ := io.ReadAll(r)
+	_, err := s.store.Write(key, bytes.NewReader(b))
+	if err != nil {
+		return err
+	}
+	if err := s.broadcastNetwork(key, b); err != nil {
+		return err
+	}
 	return nil
 }
